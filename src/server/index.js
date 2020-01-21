2c99f8520d61d18e37cd054e9bc4bab3412c2490
Timofei <timophey01@gmail.com>
2020-01-21 15:26:17 +0700

    fixes index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const { ApiServer } = require('./api-server');
-const { ResourceUnavailableError } = require('./resource-error');
+const ResourceUnavailableError = require('./resource-error');
+const HttpError = require('./http-error');
 
-module.exports = { ApiServer, ResourceUnavailableError };
+module.exports = { ApiServer, ResourceUnavailableError, HttpError };
