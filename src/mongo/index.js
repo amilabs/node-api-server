5614670b44dfb1993fd33e89472a53c8b65b4fc7
Timofei <timophey01@gmail.com>
2020-01-21 15:36:30 +0700

    timeline to index.js

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -1,9 +1,11 @@
 const { MongoCollection } = require('./mongo-collection');
+const { Timeline } = require('./timeline');
 const { mongoInit } = require('./mongo-init');
 const ValidationError = require('./errors/validation');
 
 module.exports = {
     MongoCollection,
+    Timeline,
     mongoInit,
     ValidationError
 };
